6e0bc685697ef753f63848b51ca4443e72ed649f
Santiago Astua <sastuae@ucenfotec.ac.cr>
2024-10-16 20:31:13 -0600

    Test1

--- Bullet.cs
+++ Bullet.cs
@@ -1,17 +1,24 @@
 using Godot;
 using System;
 
-public partial class Enemigo1 : CharacterBody2D
+public partial class Bullet : Area2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+	}
+	
+	private void _on_Bullet_body_entered(Node body)
+	{
+		if (body.IsInGroup("mobs"))
+		{
+			body.QueueFree();
+		}
+		QueueFree();
 	}
 }
--- Defence1.cs
+++ Defence1.cs
@@ -1,12 +1,11 @@
 using Godot;
 using System;
 
-public partial class NewScript : Node
+public partial class Defence1 : StaticBody2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
