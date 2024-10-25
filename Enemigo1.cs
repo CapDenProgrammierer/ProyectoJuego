47cd37c5fc9bfae33752d7deb1bba4f5b841d957
Santiago <147957491+CapDenProgrammierer@users.noreply.github.com>
2024-10-24 22:22:06 -0600

    Cambio
    si

--- Enemigo1.cs
+++ Enemigo1.cs
@@ -6,12 +6,12 @@ public partial class Enemigo1 : CharacterBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		$AnimationPlayer.play(AnimacionPrueba);
+		Console.WriteLine("si");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var direccion = Input.get_vector("ui_left", "")
+		
 	}
 }
